a0c12bd3fdf0510a70fedc8a68a937effdbe9b8e
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-10-14 10:39:44 +0200

    Cleanup of the code

--- game.py
+++ game.py
@@ -1,51 +1,50 @@
 # -*- coding: utf-8 -*-
 import io, re
 from networkx import MultiDiGraph
-#from oauthlib.uri_validate import path
 
 class game(MultiDiGraph):
     
-    #The class game herit from the class DiGraph
     def __init__(self):
+        '''The class game inherit from the class DiGraph and initialize a new game'''
+        
         super().__init__()
-    
-    #This method take the name of the file in input, then parse it and convert it into a directed graph
+
+
     def importfile(self, graphfile):
+        '''This method take the name of the file in input, then parse it and convert it into a directed graph'''
+        
         with io.open(graphfile, 'r') as graphtxt: #Open the file
             if graphtxt.readable(): #Check if we can read it
                 size = int(graphtxt.readline()[0]) #Get the first line which is the size of the array
                 self.add_nodes_from(range(1,size+1)) #Create as much node as the size of the array
-                #print(self.nodes())
                 rx = re.compile('[^0-9]') #Regular expression to keep only the numbers
                 for i in range(1,size+1): #Then parse the file line by line to create the edges
                     curline = graphtxt.readline()
                     curline = rx.sub('', curline).strip() #Apply the regular expression on the current line
-                    #print(curline)
                     for j in range(1,size+1):
                         connect = int(curline[j-1])
                         if connect:
-                            #print('pour j =', j, 'et i = ', i, ' connect vaut ' ,connect)
                             self.add_edge(i, j)
-        return None
-                            
-    #Algorithm who create the list of levels we will use the Kosaraju’s algorithm
-    #It works very simply : 
-    #   1- We do a dfs of the graph and we put all the vertexes at the end on an empty stack
-    #   2- We reverse the direction of the edges (so this new graph have the same strongly connected components)
-    #   3- We do an other dfs on this new graph beginning with the node on the top of the stack
-    #   4- When we reach an end, all the traversed nodes are strongly connected. We pop them out of the stack
+
+
     def levels(self):
+        '''Algorithm who create the list of levels we will use the Kosaraju’s algorithm
+        This is is how it works : 
+            1- We do a dfs of the graph and we put all the vertexes at the end on an empty stack
+            2- We reverse the direction of the edges (so this new graph have the same strongly connected components)
+            3- We do an other dfs on this new graph beginning with the node on the top of the stack
+            4- When we reach an end, all the traversed nodes are strongly connected. We pop them out of the stack'''
+        
         stack = self.dfs(1)
-        #print(stack)
-        #print(self.edges())
         self.reverse(False)
-        #print(self.edges())
         components = self.strongdetection(stack)
         self.reverse(False)
         return components
 
-    #Recursive depth-first traversal and store the nodes in the reverse order on a stack
+
     def dfs(self, startnode, stack = list(), revstack = list()):
+        '''Recursive depth-first traversal, it store the nodes on a stack'''
+
         successors = self.successors(startnode)
         stack.append(startnode)
         if len(successors) != 0:
@@ -54,23 +53,30 @@ class game(MultiDiGraph):
                     self.dfs(succ, stack)
         revstack.append(startnode)
         return revstack
-    
-    #The two following methods search all the strongly connected components
+
+
     def strongdetection(self, dfs_stack, stack=list()):
+        '''List all the strongly components while they are nodes'''
         while len(dfs_stack) > 0:
             stack.append(self.strongcomponent(dfs_stack.pop(), dfs_stack, []))
         
         return stack
 
+
     def strongcomponent(self, startnode, dfs_stack, currstrong):
+        '''Find a strongly connected component containing the given vertex'''
+        
         currstrong.append(startnode)
         for succ in self.successors(startnode):
             if dfs_stack.count(succ) > 0:
                 self.strongcomponent(dfs_stack.pop(dfs_stack.index(succ)), dfs_stack, currstrong)
         
         return currstrong
-    
+
+
     def reducedgraph(self, strongcomponents):
+        '''Generate the reduced graph from the strong components'''
+
         newgraph = game()
         i = 1
         for currcomponent in strongcomponents:
@@ -85,8 +91,11 @@ class game(MultiDiGraph):
             i+=1
             
         return newgraph
-    
+
+
     def generatematrix(self):
+        '''Generate a matrix for any given graph'''
+        
         size = len(self.nodes())
         matrix = [[0 for j in range(size)] for i in range(size)]
         
@@ -96,8 +105,11 @@ class game(MultiDiGraph):
             matrix[i-1][j-1] += 1
                 
         return matrix
-    
+
+
     def longestpath(self, startnode, endnode, path = []):
+        '''Find the longest path by testing all the possibilities'''
+
         if path.count(startnode) == 0:
             path.append(startnode)
             if startnode != endnode:
@@ -117,8 +129,3 @@ class game(MultiDiGraph):
             return path
         else:
             return False
-    
-    '''def longestpath(self, startnode, endnode, edges = []):
-        if len(edges) == 0:
-            edges = self.edges()'''
-        
--- start.py
+++ start.py
@@ -1,20 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
-sys.path.append('networkx-1.10-py2.7.egg')
-sys.path.append('decorator-4.0.4-py2.py3-none-any.whl')
-import networkx
 from game import game
 
 if __name__ == "__main__":
     G=game()
     G.importfile('mat1.txt')
 
-    print("Integrated algorithm :")
-
-    test = networkx.strongly_connected_components(G)
-    for c in test:
-        print(c)
-        
     print("My algorithm")
         
     strongcomp = G.levels()
