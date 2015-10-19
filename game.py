14c46e72feee4b325783ad057afc79084e3f80ae
Paul TREHIOU <paul.trehiou@utbm.fr>
2015-10-19 15:20:28 +0200

    More comments

--- game.py
+++ game.py
@@ -3,10 +3,10 @@ import io, re
 from networkx import MultiDiGraph
 
 class game(MultiDiGraph):
-    
+
     def __init__(self):
         '''The class game inherit from the class DiGraph and initialize a new game'''
-        
+
         super().__init__()
 
 
@@ -35,10 +35,10 @@ class game(MultiDiGraph):
             3- We do an other dfs on this new graph beginning with the node on the top of the stack
             4- When we reach an end, all the traversed nodes are strongly connected. We pop them out of the stack'''
         
-        stack = self.dfs(1)
-        self.reverse(False)
-        components = self.strongdetection(stack)
-        self.reverse(False)
+        stack = self.dfs(1) #Do a first dfs
+        self.reverse(False) #Reverse the graph
+        components = self.strongdetection(stack) #Launch the detection of strongly connected components
+        self.reverse(False) #Reverse the graph again to have the same
         return components
 
 
@@ -46,16 +46,16 @@ class game(MultiDiGraph):
         '''Recursive depth-first traversal, it store the nodes on a stack'''
         
         if stack == None:
-            stack = list()
+            stack = list() #Initialise an empty stack if none is provided
         if revstack == None:
             revstack = list()
-        successors = self.successors(startnode)
-        stack.append(startnode)
-        if len(successors) != 0:
+        successors = self.successors(startnode) #Get the successors
+        stack.append(startnode) #Add this node to the stack
+        if len(successors) != 0: #Go as deepest as possible for each node
             for succ in successors:
                 if stack.count(succ) == 0:
                     revstack = self.dfs(succ, stack, revstack)
-        revstack.append(startnode)
+        revstack.append(startnode) #If we reach this line that mean we are blocked at a end
         return revstack
 
 
@@ -64,17 +64,20 @@ class game(MultiDiGraph):
         
         if stack == None:
             stack = list()
-        while len(dfs_stack) > 0:
-            stack.append(self.strongcomponent(dfs_stack.pop(), dfs_stack, []))
+        while len(dfs_stack) > 0: #While the stack is not empty there are strongly connected components
+            stack.append(self.strongcomponent(dfs_stack.pop(), dfs_stack))
         
         return stack
 
 
-    def strongcomponent(self, startnode, dfs_stack, currstrong):
+    def strongcomponent(self, startnode, dfs_stack, currstrong=None):
         '''Find a strongly connected component containing the given vertex'''
         
+        if currstrong == None:
+            currstrong = list()
+
         currstrong.append(startnode)
-        for succ in self.successors(startnode):
+        for succ in self.successors(startnode): #All the successors that are still in the stack are strongly connected
             if dfs_stack.count(succ) > 0:
                 self.strongcomponent(dfs_stack.pop(dfs_stack.index(succ)), dfs_stack, currstrong)
         
@@ -82,7 +85,8 @@ class game(MultiDiGraph):
 
 
     def reducedgraph(self, strongcomponents = None):
-        '''Generate the reduced graph from the strong components'''
+        '''Generate the reduced graph from the strong components
+           This create a new graph were each node represent a strongly connected component'''
         
         if strongcomponents == None:
             strongcomponents = self.levels()
@@ -121,22 +125,22 @@ class game(MultiDiGraph):
         
         if path == None:
             path = list()
-        if path.count(startnode) == 0:
-            path.append(startnode)
-            if startnode != endnode:
-                maxlen = 0
+        if path.count(startnode) == 0: #Check if the current node has already been traversed
+            path.append(startnode) #We append the current node to the stack
+            if startnode != endnode: #Check if we are at the end
+                maxlen = 0 #if not, find the longest path that begin with the current node
                 maxpath = path
-                for succ in self.successors(startnode):
+                for succ in self.successors(startnode): #Recursive call to get the longest path that begin with each successors
                     newpath = self.longestpath(succ, endnode, path[:])
-                    if newpath != False:
-                        newlen = len(newpath)
-                        if newlen > maxlen:
+                    if newpath != False: #If there is a path beggining with this successors
+                        newlen = len(newpath) #We get the size
+                        if newlen > maxlen: #Check if it is longer than any previous attempt
                             maxlen = newlen
                             maxpath = newpath
                             
-                path = maxpath
+                path = maxpath #Save the longest path
                 
-        if path[-1] == endnode:
+        if path[-1] == endnode: #Check if the last node is effectively the right node (so if the path is valid)
             return path
         else:
-            return False
+            return False #If not return false
